ad2fdbdb9faa19dad3aaa1f10ee5eb74c4f539c8
LeeJunSung <jun130506@gmail.com>
2025-12-10 21:34:41 +0900

    자산 목록 수정

--- src/main/java/com/project/coinTrade/controller/AssetController.java
+++ src/main/java/com/project/coinTrade/controller/AssetController.java
@@ -3,17 +3,18 @@ package com.project.coinTrade.controller;
 import com.project.coinTrade.service.AssetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/assets")
+@RequestMapping("/coin")
 public class AssetController {
 
     @Autowired
     AssetService assetService;
 
-    @GetMapping("/getTotalAccounts")
+    @PostMapping("/assets")
     public Object getTotalAccounts() throws Exception {
         return assetService.getTotalAccounts();
     }
